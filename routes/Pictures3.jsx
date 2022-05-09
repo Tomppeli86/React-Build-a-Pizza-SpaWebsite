7aef0dc6b9fb0148172801b9a96f78ef82c83184
Tomppeli86 <99166139+Tomppeli86@users.noreply.github.com>
2022-05-09 15:31:45 +0300

    Update Pictures3.jsx

--- routes/Pictures3.jsx
+++ routes/Pictures3.jsx
@@ -8,19 +8,24 @@ export default function Pictures3() {
   
   const [stuff, setStuff] = useState( [  ] )
 
+  
+  const orderPizza = []
+
+
   const hook = () => {
     axios
       .get('http://localhost:3001/stuff')
       .then(response => {
         console.log('promise fulfilled')
         setStuff(response.data)
-        console.log(response.data)
+
      })  
   }
   useEffect(hook, [])
 
 
-  
+
+
   const quickAndDirtyStyle = {
     width: "50px",
     height: "50px",
@@ -31,77 +36,30 @@ export default function Pictures3() {
     alignItems: "center"
   };
   
+
+  
+
   const id = (x) => x;
-  
-  // complex logic should be a hook, not a component
+
   const useDraggable = ({ onDrag = id } = {}) => {
-    // this state doesn't change often, so it's fine
     const [pressed, setPressed] = useState(false);
-  
-    // do not store position in useState! even if you useEffect on
-    // it and update `transform` CSS property, React still rerenders
-    // on every state change, and it LAGS
     const position = useRef({ x: 0, y: 0 });
     const ref = useRef();
-  
-    // handlers must be wrapped into `useCallback`. even though
-    // resubscribing to `mousedown` on every tick is quite cheap
-    // due to React's event system, `handleMouseDown` might be used
-    // in `deps` argument of another hook, where it would really matter.
-    // as you never know where return values of your hook might end up,
-    // it's just generally a good idea to ALWAYS use `useCallback`
     const handleMouseDown = useCallback((e) => {
-      // don't forget to disable text selection during drag and drop
-      // operations
       e.target.style.userSelect = "none";
       setPressed(true);
     }, []);
   
-    // TODO:
-    // we've moved the code into the hook, and it would be weird to
-    // return `ref` and `handleMouseDown` to be set on the same element
-    // why not just do the job on our own here and use a function-ref
-    // to subscribe to `mousedown` too? it would go like this:
-    // const ref = useRef();
-    // const unsubscribe = useRef();
-    // const legacyRef = useCallback((elem) => {
-    //   ref.current = elem;
-    //   if (unsubscribe.current) {
-    //     unsubscribe.current();
-    //   }
-    //   if (elem) {
-    //     elem.addEventListener('mousedown', handleMouseDown);
-    //     unsubscribe.current = () => {
-    //       elem.removeEventListener('mousedown', handleMouseDown);
-    //     };
-    //   }
-    // }, []);
-    // then keep using `ref` to the end of this hook, but
-    // return a `[legacyRef, pressed]`
-  
     useEffect(() => {
       if (!pressed) {
+        
         return;
       }
-      // subscribe to mousemove only when pressed, otherwise it will lag
-      // even when you're not dragging
-      // TODO: updating the page without any throttling is a bad idea
-      // requestAnimationFrame-based throttle would probably be fine,
-      // but be aware that naive implementation might make element
-      // lag 1 frame behind cursor, and it will appear to be lagging
-      // even at 60 FPS
       const handleMouseMove = (event) => {
-        // needed for TypeScript anyway
         if (!ref.current || !position.current) {
           return;
         }
         const pos = position.current;
-        // it's important to save it into variable here,
-        // otherwise we might capture reference to an element
-        // that was long gone. not really sure what's correct
-        // behavior for a case when you've been scrolling, and
-        // the target element was replaced. probably some formulae
-        // needed to handle that case. TODO
         const elem = ref.current;
         position.current = onDrag({
           x: pos.x + event.movementX,
@@ -113,35 +71,55 @@ export default function Pictures3() {
         e.target.style.userSelect = "auto";
         setPressed(false);
       };
-      // subscribe to mousemove and mouseup on document, otherwise you
-      // can escape bounds of element while dragging and get stuck
-      // dragging it forever
       document.addEventListener("mousemove", handleMouseMove);
       document.addEventListener("mouseup", handleMouseUp);
       return () => {
+
+
+        setIt(position.current, ref)
+        
+        
         document.removeEventListener("mousemove", handleMouseMove);
         document.removeEventListener("mouseup", handleMouseUp);
       };
-      // if `onDrag` wasn't defined with `useCallback`, we'd have to
-      // resubscribe to 2 DOM events here.
     }, [pressed, onDrag]);
-  
-    // actually it makes sense to return an array only when
-    // you expect that on the caller side all of the fields
-    // will be usually renamed
     return [ref, pressed, handleMouseDown];
-  
-    // > seems the best of them all to me
-    // this code doesn't look pretty anymore, huh?
   };
   
+
+
+
+
+
+
+  const setIt = (posfill, namefill) => {
+    
+    const newPosition = {name: namefill, position: posfill}
+    let existingFilling = orderPizza.filter(filling => filling.name === newPosition.name)[0]
+
+    console.log(namefill)
+    console.log(posfill)
+  
+
+    if (existingFilling) {
+        
+        console.log(newPosition)
+        orderPizza.filter((item) => item.name !== namefill)
+        orderPizza.push(newPosition)
+        console.log(orderPizza)
+      
+    } else {
+
+        orderPizza.push(newPosition)
+        console.log(orderPizza)
+
+      }
+}
   
   
   
   const DraggableComponent = (props) => {
-    
-    console.log(props.aines)
-  
+
     const handleDrag = useCallback(
       ({ x, y }) => ({
         x: Math.max(-200, x),
@@ -152,19 +130,20 @@ export default function Pictures3() {
     );
   
     const [ref, pressed, handleMouseDown] = useDraggable({
-      onDrag: handleDrag
+      onDrag: handleDrag 
     });
 
+    
+
     return (
-      <div className="tomato">
-  
+      <div className="draggables">
+
             <img src={"/kuvat/"+ props.aines +".png"} className={props.aines}
               ref={ref} onMouseDown={handleMouseDown} style={quickAndDirtyStyle}/> 
-           
       </div>
-      
     );
   };
+  
   
 
   return (
@@ -172,26 +151,31 @@ export default function Pictures3() {
     <div>
           
           <div className="chosenfillings">
-                <p>Fillit'</p>
 
                 <div>
-                    {stuff.map((fill) => ( 
-
-                          <DraggableComponent aines={fill.name}/> 
-
-                        ))} 
-                    
-      
+                 {stuff.map((fill) => ( 
+                          <DraggableComponent aines={fill.name}/>
+                        ))}
+                {stuff.map((fill) => ( 
+                          <DraggableComponent aines={fill.name}/>
+                        ))}
+                {stuff.map((fill) => ( 
+                          <DraggableComponent aines={fill.name}/>
+                        ))}
                     {console.log("dragged")}
                 </div>
-                   
+                  
+                <div><Link to="/Pictures2"list={orderPizza} class="linktxt">Order</Link></div>
           </div>
+          
 
 
           <img src="/kuvat/pizza.png" className="bigpic"/>  
              
-            <p class="pictxt"> <Link to="/Koti" class="linktxt">Back</Link>
-            </p>     
+            <p class="pictxt"> <Link to="/Koti" class="linktxt">Backza</Link> 
+            </p> <br></br>
+       
+               
     </div>
 
   );
